d19edb0cf006c467b1b6c8adab7b607b8cd85040
jcabo65 <jcabo65@gmail.com>
2025-10-26 04:04:56 +0000

    Ajustar ruta en autoloader.php a ../src/ sin modificar código

--- web/autoloader.php
+++ web/autoloader.php
@@ -1,10 +1,1 @@
-<?php
-
-spl_autoload_register(
-    function ($className)
-    {
-        if (stream_resolve_include_path($file = ("src/" . str_replace("\\", "/", $className) . ".php"))) {
-            include $file;
-        }
-    }
-);
+spl_autoload_register(function ($className) { if (stream_resolve_include_path($file = ("../src/" . str_replace("\\", "/", $className) . ".php"))) { include $file; } });
